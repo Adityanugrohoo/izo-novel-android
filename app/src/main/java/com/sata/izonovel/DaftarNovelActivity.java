a88c5603aeb5a01b95d3e68a69dab73109cf3957
Adityanugrohoo <a.nugrohooooo@gmail.com>
2023-05-13 16:06:04 +0700

    Menambahkan Daftar Novel activity_daftar_novel.xml Menambahkan Main Novel activity_main.xml Menambahkan Menu Favorite activity_menu_favorite.xml
    UTS ADITYA DWI NUGROHO
    NIM 210444020026
    MANAJEMEN INFORMATIKA

--- app/src/main/java/com/sata/izonovel/DaftarNovelActivity.java
+++ app/src/main/java/com/sata/izonovel/DaftarNovelActivity.java
@@ -2,13 +2,28 @@ package com.sata.izonovel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class DaftarNovelActivity extends AppCompatActivity {
+    LinearLayout lanjut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar_novel);
+
+        lanjut = findViewById(R.id.lanjut);
+        lanjut.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                Intent intent = new Intent(DaftarNovelActivity.this, MenuFavoriteActivity.class);
+                startActivity(intent);
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/com/sata/izonovel/MainActivity.java
+++ app/src/main/java/com/sata/izonovel/MainActivity.java
@@ -7,10 +7,18 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     LinearLayout menuInfoPengguna;
+    LinearLayout menudaftarnovel;
+
+    LinearLayout novel;
+
+
+
+    TextView moreFavorite;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +32,33 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 return false;
             }
+
         });
+        menudaftarnovel = findViewById(R.id.menu_one);
+        menudaftarnovel.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                Intent intent = new Intent(MainActivity.this, DaftarNovelActivity.class);
+                startActivity(intent);
+                return false;
+            }
+
+        });
+
+        novel = findViewById(R.id.menufav);
+        novel.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                Intent intent = new Intent(MainActivity.this, DaftarNovelActivity.class);
+                startActivity(intent);
+                return false;
+            }
+
+        });
+
+
+
+
+
     }
 }
